c08dd8c691addaa723f038d2cc427fce15023fdf
Tyler Green <tyler.green2@gmail.com>
2013-09-16 15:22:04 -0500

    added carousel to homepage

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -3,7 +3,7 @@ function partials(file){
 }
 
 
-var App = angular.module('App', ['firebase']).
+var App = angular.module('App', ['firebase', 'ui.bootstrap']).
 	config(['$routeProvider', function($routeProvider){
 				$routeProvider.
 					when('/', { templateUrl: partials('home.html'), controller: HomeCtrl } ).
@@ -13,8 +13,24 @@ var App = angular.module('App', ['firebase']).
 			
 ]);
 
-function HomeCtrl($scope){
-}
+function HomeCtrl($scope) {
+  $scope.myInterval = 6000;
+  var slides = $scope.slides = [];
+  $scope.addSlide = function() {
+	  
+    var newWidth = 200 + ((slides.length + (25 * slides.length)) % 150);
+    slides.push({
+      image: 'http://placekitten.com/' + newWidth + '/200',
+      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+    });
+  };
+  for (var i=0; i<4; i++) {
+    $scope.addSlide();
+  }
+};
+
+
 
 // App.constant('decksUrl', 'https://greent.firebaseio.com/skateshop/products/decks');
 App.constant('decksUrl', 'https://greent.firebaseio.com/mydecks');
--- javascripts/vendor/ui-bootstrap-tpls-0.6.0.js
+++ javascripts/vendor/ui-bootstrap-tpls-0.6.0.js
@@ -3333,9 +3333,6 @@ angular.module("template/alert/alert.html", []).run(["$templateCache", function(
 angular.module("template/carousel/carousel.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("template/carousel/carousel.html",
     "<div ng-mouseenter=\"pause()\" ng-mouseleave=\"play()\" class=\"carousel\">\n" +
-    "    <ol class=\"carousel-indicators\" ng-show=\"slides().length > 1\">\n" +
-    "        <li ng-repeat=\"slide in slides()\" ng-class=\"{active: isActive(slide)}\" ng-click=\"select(slide)\"></li>\n" +
-    "    </ol>\n" +
     "    <div class=\"carousel-inner\" ng-transclude></div>\n" +
     "    <a ng-click=\"prev()\" class=\"carousel-control left\" ng-show=\"slides().length > 1\">&lsaquo;</a>\n" +
     "    <a ng-click=\"next()\" class=\"carousel-control right\" ng-show=\"slides().length > 1\">&rsaquo;</a>\n" +
